1f5a3849718f9e88b1772a9137693de46b942110
Nik <anonnik5@gmail.com>
2021-05-25 15:38:59 +0300

    main: deletr deploy

--- deploy.bat
+++
@@ -1,1 +0,0 @@
-%msdeployv2% -verb:sync -source:iisapp="%WORKSPACE%" -dest:iisapp="avs4you.teamlab.info\blog",computerName="35.174.218.204",username=%deploymentuser%,password=%deploymentpass% -skip:file=deploy.bat -skip:file=Jenkinsfile -skip:Directory=".git"
